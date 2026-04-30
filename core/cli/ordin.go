ecd77368d762ff347a25493a187efd0940ab127a
Purushotham <yallurupurushothamreddy006@gmail.com>
2026-04-30 11:28:53 +0530

    rbe/parser added cli for checking rule config

---
+++ core/cli/ordin.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"ordin/m/core/rule_engine"
+	"os"
+	"strings"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+
+	cmd := &cli.Command{
+		Name:    "ordin",
+		Usage:   "sort your filez",
+		Version: "0.1",
+
+		Commands: []*cli.Command{
+			{
+				Name:    "check",
+				Usage:   "to validate rules config",
+				Aliases: []string{"c"},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					val := strings.TrimSpace(cmd.Args().First())
+					if val != "" {
+						err := rule_engine.Parse_validate_pipeline(val)
+						if err != nil {
+							return err
+						} else {
+							fmt.Println("Good to go!")
+							return nil
+						}
+					}
+					return fmt.Errorf("Invalid argument")
+				},
+			},
+		},
+
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Printf("Ordin Version: %s\n", cmd.Version)
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
