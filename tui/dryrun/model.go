5bd7b3d4c8b8578e1353bc90f28f8c272a963ef5
Purushotham <yallurupurushothamreddy006@gmail.com>
2026-05-18 23:46:36 +0530

    modified dry run tui to account for multiple moves

---
+++ tui/dryrun/model.go
@@ -0,0 +1,78 @@
+package dryrun
+
+import (
+	"fmt"
+	"strings"
+
+	"ordin/rules"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+func initializeModel(pathActions map[string]*rules.PathAction) model {
+	displayPathActions := []displayPathAction{}
+	for _, pathAction := range pathActions {
+		displayPathActions = append(displayPathActions, getDisplayPathAction(*pathAction))
+	}
+	return model{displayPathActions: displayPathActions, cursor: 0}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if m.displayPathActions[m.cursor].focused {
+		updated, cmd := m.displayPathActions[m.cursor].Update(msg)
+
+		m.displayPathActions[m.cursor] = updated.(displayPathAction)
+
+		switch msg := msg.(type) {
+		case tea.KeyPressMsg:
+			if msg.String() == "esc" {
+				m.displayPathActions[m.cursor].focused = false
+			}
+		}
+
+		return m, cmd
+
+	}
+
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "up", "j":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "k":
+			if m.cursor < len(m.displayPathActions)-1 {
+				m.cursor--
+			}
+		case "space":
+			m.displayPathActions[m.cursor].focused = true
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "ctrl+s", "s":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	s := strings.Builder{}
+	for i, displayPathAction := range m.displayPathActions {
+		cursor := " "
+		if i == m.cursor {
+			cursor = ">>"
+		}
+		delStatus := "DISABLED"
+		if displayPathAction.toDelete {
+			delStatus = "ENABLED"
+		}
+		fmt.Fprintf(&s, "%s %s %s/n%s/n", cursor, displayPathAction.Srcpath, delStatus, displayPathAction.View().Content)
+	}
+	return tea.NewView(s.String())
+}
